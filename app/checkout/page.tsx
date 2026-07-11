855e123615a90526f1e2bf9e6a340c3437f22d87
SmileyGz <89216465+SmileyGz@users.noreply.github.com>
2026-07-11 18:06:59 -0500

    feat: expose database error on checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -159,7 +159,7 @@ export default function CheckoutPage() {
           
           window.location.href = data.init_point
         } else {
-          toast.error(data.mp_error ? `Error MercadoPago: ${data.mp_error}` : 'Hubo un error al generar el pago. Intenta de nuevo.')
+          toast.error(data.sb_error ? `Error DB: ${data.sb_error}` : data.mp_error ? `Error MercadoPago: ${data.mp_error}` : data.error || 'Hubo un error al generar el pago. Intenta de nuevo.')
           console.error(data)
         }
       }
